32775d77d67a45a314391177707a6c34d2b398a6
“Philippe <philippe.henry@edu.esiee.fr>
2022-03-20 12:29:28 +0100

    [mod] Modification of the main by adding the joke list and linking it to the button. Now when you click on the button you will have a random joke on your screen

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,7 +6,7 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
 import java.net.URL
-
+import kotlin.random.Random
 
 
 class MainActivity : AppCompatActivity() {
@@ -18,15 +18,30 @@ class MainActivity : AppCompatActivity() {
         val btn_click_me = findViewById(R.id.button) as Button
         val textView = findViewById(R.id.textView3) as TextView
         var timesclick = ""
+        val Joke_list = listOf("Those aren't credits that roll after Walker Texas Ranger. It is actually a list of fatalities that occurred during the making of the episode.",
+        "The devil has a Chuck Norris-may-care attitude.", "Chuck Norris roundhouse kicked the winnyness out of Caillou.", "The Most Interesting Man in the World's hair is not actually grey. It's actually covered with Chuck Norris' sperm.",
+        "Freddy thought he was the true nightmare until he met Chuck Norris who roundhouse kicked and from that day Freddy hides in fear thinking a nightmare in texas", "Chuck Norris can drink a bucket of KFC.",
+        "Mike Tyson was overheard saying he would bite a live cobra before he would bite Chuck Norris' ear.", "Chuck Norris often works out in public. He was spotted last week on Hwy 66 roundhouse kicking tornados as a warm up.",
+        "Medusa's hair is made of snakes. Chuck Norris's beard is made of hair-colored barbed wire. Medusa can turn you into stone by staring at you. Chuck Norris doesn't have to look at you to turn you into pulp.",
+        "When Chuck Norris stares at you your balls drop to the ground", "Chuck Norris is the only person on Earth who can literally be beside himself. You don't want to be around when they do that.",
+        "Chuck Norris smelly breath is so strong that he can demolish a house just by breathing on it.")
+
+
+
+
 
 
 
             // apl d'api via requête
         btn_click_me.setOnClickListener{
             //getJsonFromURL("https://api.chucknorris.io/jokes/random")
-            timesclick = "blabla";
-            textView.text = timesclick.toString();
-            Toast.makeText(this@MainActivity,"click slp", Toast.LENGTH_SHORT).show()
+
+            var randomIndex = Random.nextInt(Joke_list.size);
+            var randomElement = Joke_list[randomIndex]
+
+            randomElement = randomElement;
+            textView.text = randomElement.toString();
+            Toast.makeText(this@MainActivity,"Another Joke ? :D", Toast.LENGTH_SHORT).show()
         }
     }
 
